7b919a6604df098d6f535f57a16f0672593b79ab
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2015-02-05 21:42:17 -0800

    Add navbar with login and signup link

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -1,11 +1,21 @@
 var app = angular.module('getHired');
-app.config(['$stateProvider', '$urlRouterProvider' function($stateProvider, $urlRouterProvider) {
-  $routeProvider.
-    when('/', {
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
       templateUrl: "home/index.html",
-      controller: "HomeController"
-    }).
-    otherwise({
-      redirectTo: '/'
+      controller: "HomeController",
+      views: {
+        "nav": {
+          templateUrl: "nav/navbar.html"
+        }
+      }
+    })
+    .state('login', {
+      url: '/login'
+    })
+    .state('sign-up', {
+      url: '/sign-up'
     });
+  $urlRouterProvider.otherwise('/');
 }]);
