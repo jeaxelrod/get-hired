7296534feca212b28e99b514ef672c90316ff939
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2015-02-11 19:42:00 -0800

    Add implementation of AuthService tests

--- spec/javascripts/services/AuthService_spec.js
+++ spec/javascripts/services/AuthService_spec.js
@@ -6,12 +6,28 @@ describe("Angular authentication service, AuthService,", function() {
   beforeEach(module("getHired"));
 
   beforeEach(inject(function(_AuthService_) {
-    AuthSerice = _AuthService_;
+    AuthService = _AuthService_;
   }));
 
-  it("should correct set currentUser");
-  it("should retreive the currentUser");
-  it("should correctly state when a user isn't logged in");
-  it("should correctly state when a user is logged in");
+
+  it("should retreive the currentUser", function() {
+    expect(AuthService.currentUser()).toBe(undefined);
+  });
+
+  it("should correctly set currentUser", function() {
+    var user = {"email": "john@gmail.com", "password": "password"}
+    AuthService.setCurrentUser(user);
+    expect(AuthService.currentUser()).toBe(user);
+  });
+
+  it("should correctly state when a user isn't logged in", function() {
+    expect(AuthService.isLoggedIn()).toBe(false);
+  });
+  it("should correctly state when a user is logged in", function() {
+    var user = {"email": "john@gmail.com", "password": "password"}
+    AuthService.setCurrentUser(user);
+    expect(AuthService.isLoggedIn()).toBe(true);
+  });
+
   it("should retreive the currentUser from API");
 });
