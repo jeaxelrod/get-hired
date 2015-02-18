05a8c639dc3a542fc68359e095121200bb185d35
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2015-02-17 20:10:34 -0800

    JobsIndex controller gets and filters the jobs data

---
+++ app/assets/javascripts/controllers/jobs/jobs_index_controller.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var app = angular.module("getHired");
+
+app.controller("JobsIndexController", ["$scope", "$http",
+  function($scope, $http) {
+    $http.get('/user/jobs').
+      success(function(data, status, headers, config) {
+        console.log("hi");
+        $scope.jobs = data.map(function(job) {
+          return {
+            position: job.position,
+            company:  job.company,
+            link:     job.link
+          }
+        });
+      }).
+      error(function(data, status, headers, config) {
+      });
+  }
+ ]);
+
--- spec/javascripts/controllers/jobs_index_controller_spec.js
+++ spec/javascripts/controllers/jobs_index_controller_spec.js
@@ -1,22 +1,26 @@
 "use strict";
 
 describe("JobsIndexController", function() {
-  var scope, controller, $httpBackend;
+  var scope, controller, $httpBackend, jobs;
 
   beforeEach(module('getHired'));
 
   beforeEach(inject(function($rootScope, $controller, _$httpBackend_) {
     scope = $rootScope.$new();
     $httpBackend = _$httpBackend_;
+    jobs = [{ id: 1, position: "Position 1", company: "Company 1", link: "http://link1.com" },
+                { id: 2, position: "Position 2", company: "Company 2", link: "http://link2.com"}]
+    $httpBackend.expectGET("/user/jobs").
+      respond(jobs);
     controller = $controller("JobsIndexController", { $scope: scope});
   }));
 
   it("should lists all jobs", function() {
-    var jobs = [{ position: "Position 1", company: "Company 1", link: "http://link1.com" },
-                { position: "Position 2", company: "Company 2", link: "http://link2.com"}]
-    $httpBackend.expectGET("user/jobs").
-      respond(jobs);
     $httpBackend.flush();
-    expect(scope.jobs).toEqual(jobs);
+    var filteredJobs = [
+      { position: jobs[0].position, company: jobs[0].company, link: jobs[0].link },
+      { position: jobs[1].position, company: jobs[1].company, link: jobs[1].link }
+    ];
+    expect(scope.jobs).toEqual(filteredJobs);
   });
 });
