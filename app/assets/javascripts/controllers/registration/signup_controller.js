5aed67b079c75027cdc79837ff6b2f287ea8fc27
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2015-02-17 17:01:49 -0800

    Delete unneeded console.logs

--- app/assets/javascripts/controllers/registration/signup_controller.js
+++ app/assets/javascripts/controllers/registration/signup_controller.js
@@ -6,7 +6,6 @@ app.controller("SignupController", ["$scope", "$http", "$location", "FlashServic
   function($scope, $http, $location, FlashService, AuthService) {
     $scope.user = {email: "", password: "", password_confirmation: ""};
     $scope.signup = function(user) {
-      console.log("User: ", user);
       user.password_confirmation = user.password_confirmation ? 
                                      user.password_confirmation : 
                                      "";
@@ -15,13 +14,11 @@ app.controller("SignupController", ["$scope", "$http", "$location", "FlashServic
                                          password_confirmation: user.password_confirmation }}).
 
         success(function(data, status, headers, config) {
-          console.log("Success data: ", data);
           FlashService.addMessage({message: "Welcome to Get Hired", type: "success"});
           AuthService.setCurrentUser(data);
           $location.path("");
         }).
         error(function(data, status, headers, config) {
-          console.log("Failure data: ", data);
           $scope.emailMessage = data.errors.email ? 
             "Email " + data.errors.email[0] : "";
           $scope.passwordMessage = data.errors.password ? 
