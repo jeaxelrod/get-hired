676e62463d6033c3f67a4d2a29ab58de203a7f35
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2015-02-18 16:53:17 -0800

    Add feature test to test that navbar links become active if page is on those links

---
+++ spec/features/navbar_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.feature "Navbar", :type => :feature, js: true do
+  scenario "highlights active links" do
+    visit root_path
+    click_link "Sign In"
+    
+    within(".active") { expect(page).to have_content("Sign In") }
+  end
+end
