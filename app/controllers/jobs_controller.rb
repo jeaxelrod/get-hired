1d816115c425c742ff33c9e4a6de2eaff2175141
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2015-02-18 18:17:11 -0800

    Add API to create new jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,4 +6,11 @@ class JobsController < ApplicationController
     @jobs = current_user.jobs
     render json: @jobs 
   end
+
+  def create
+    job = params[:job]
+    respond_with current_user.jobs.create(position: job["position"], 
+                                          company:  job["company"], 
+                                          link:     job["link"]) 
+  end
 end
--- spec/requests/api/jobs_spec.rb
+++ spec/requests/api/jobs_spec.rb
@@ -1,36 +1,6 @@
 describe "Jobs API" do
   include Warden::Test::Helpers
   Warden.test_mode!
-  
-  it "should create new jobs" do
-    user = FactoryGirl.create(:user)
-    login_as(user, :scope => :user)
-    
-    post "/user/jobs", :job => { position: "Internship",
-                                 company:  "Google",
-                                 link:     "http://google.com" }
-    expect(response).to be_success
-
-    json = JSON.parse(response.body)
-    expect(json.position).to eq("Internship")
-    expect(json.company).to  eq("Google")
-    expect(json.link).to     eq("http://google.com")
-
-    job = user.jobs[0]
-    expect(job.position).to eq("Internship")
-    expect(job.company).to  eq("Google")
-    expect(job.link).to     eq("http://google.com")
-  end
-
-  it "should not let user create a new job if not logged in" do
-    post "/user/jobs", :job => { position: "Internship",
-                                 company:  "Google",
-                                 link:     "http://google.com" }
-
-    expect(response).to_not be_success
-    json = JSON.parse(response.body)
-    expect(json).to eq({"error" => "You need to sign in or sign up before continuing."})
-  end
 
   it "should return the users jobs" do
     user = FactoryGirl.create(:user)
@@ -51,6 +21,36 @@ describe "Jobs API" do
     job2 = FactoryGirl.create(:job, user: user)
 
     get "/user/jobs"
+
+    expect(response).to_not be_success
+    json = JSON.parse(response.body)
+    expect(json).to eq({"error" => "You need to sign in or sign up before continuing."})
+  end
+
+  it "should create new jobs" do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+    
+    post "/user/jobs", :job => { position: "Internship",
+                                 company:  "Google",
+                                 link:     "http://google.com" }
+    expect(response).to be_success
+
+    json = JSON.parse(response.body)
+    expect(json["position"]).to eq("Internship")
+    expect(json["company"]).to  eq("Google")
+    expect(json["link"]).to     eq("http://google.com")
+
+    job = user.jobs[0]
+    expect(job["position"]).to eq("Internship")
+    expect(job["company"]).to  eq("Google")
+    expect(job["link"]).to     eq("http://google.com")
+  end
+
+  it "should not let user create a new job if not logged in" do
+    post "/user/jobs", :job => { position: "Internship",
+                                 company:  "Google",
+                                 link:     "http://google.com" }
 
     expect(response).to_not be_success
     json = JSON.parse(response.body)
