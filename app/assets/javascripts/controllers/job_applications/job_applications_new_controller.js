0861665931a9d9095151e6bc80a2ec9e01469652
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2015-03-09 16:17:13 -0700

    Start to refactor table data into it's own Service

--- app/assets/javascripts/controllers/job_applications/job_applications_new_controller.js
+++ app/assets/javascripts/controllers/job_applications/job_applications_new_controller.js
@@ -4,19 +4,25 @@ var app = angular.module("getHired");
 
 app.controller("JobApplicationsNewController", ["$scope", "JobsService", "JobApplicationsService", "$stateParams", "$state",
   function($scope, JobsService, JobApplicationsService, $stateParams, $state) {
+    $scope.jobData = [];
     var getJobsSuccess = function(response) {
       $scope.jobs = response;
+      $scope.jobs.forEach(function(job) {
+        $scope.jobData.push({job: job});
+      });
       JobApplicationsService.setJobApplications(JobApplicationsService.getJobApplications(function(response) {
+        $scope.jobApplications = response;
         for (var i=0; i < response.length; i++) {
           var application = response[i];
           var date = new Date(application.date_applied);
-          application.formatted_date = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
-          var job = $scope.jobs.filter(function(element) {
-            return element.id == application.job_id;
+          var formatted_date = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
+          var tableRow = $scope.jobData.filter(function(element) {
+            return element.job.id === application.job_id;
           })[0];
-          job.job_application = application;
+          tableRow.job_application = application;
+          tableRow.job_application.formatted_date = formatted_date;
         }
-        $scope.jobs.filter(function(element) {
+        $scope.jobData.filter(function(element) {
           return !element.job_application
         }).map(function(element) {
           element.newApp = true;
--- app/assets/javascripts/controllers/jobs/jobs_index_controller.js
+++ app/assets/javascripts/controllers/jobs/jobs_index_controller.js
@@ -5,24 +5,29 @@ var app = angular.module("getHired");
 app.controller("JobsIndexController", ["$scope", "JobsService", "FlashService", "JobApplicationsService",
   function($scope, JobsService, FlashService, JobApplicationsService) {
     $scope.jobs = JobsService.jobs();
+    $scope.jobData = [];
     var getJobsSuccess = function(response) {
       $scope.jobs = response;
+      $scope.jobs.forEach(function(job) {
+        $scope.jobData.push({job: job});
+      });
       JobApplicationsService.setJobApplications(JobApplicationsService.getJobApplications(function(response) {
+        $scope.jobApplications = response;
         for (var i=0; i < response.length; i++) {
           var application = response[i];
           var date = new Date(application.date_applied);
-          application.formatted_date = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
-          var job = $scope.jobs.filter(function(element) {
-            return element.id == application.job_id;
+          var formatted_date = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
+          var tableRow = $scope.jobData.filter(function(row) {
+            return row.job.id === application.job_id;
           })[0];
-          job.job_application = application;
+          tableRow.job_application = application;
+          tableRow.job_application.formatted_date = formatted_date;
         }
-        $scope.jobs.filter(function(element) {
+        $scope.jobData.filter(function(element) {
           return !element.job_application
-        }).map(function(element) {
+        }).forEach(function(element) {
           element.newApp = true;
         });
-
       }));
     };
     var getJobsFailure = function(response) {
