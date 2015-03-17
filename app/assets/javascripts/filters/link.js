f06a933719572b7197ff4589d27dab6068fa7f21
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2015-03-17 16:21:04 -0700

    Change formatting of links

--- app/assets/javascripts/filters/link.js
+++ app/assets/javascripts/filters/link.js
@@ -2,8 +2,8 @@ var app = angular.module("getHired");
 
 app.filter('link', function() {
   return function(link) {
-    if (link) {
-      return link.split("://")[1];
-    }
+    var noScheme = link.split("://")[1];
+    var domain = noScheme.match(/.+\.{0,1}.+\.{1}[a-zA-Z]+/)[0];
+    return domain;
   }
 });
--- spec/javascripts/filters/filters_spec.js
+++ spec/javascripts/filters/filters_spec.js
@@ -8,11 +8,17 @@ describe("Filters", function() {
       linkFilter = _linkFilter_;
     }));
     
-    it("should format http scheme", function() {
-      expect(linkFilter("http://google.com")).toBe("google.com");
+    it("should properly format links", function() {
+      expect(linkFilter("https://www.google.com/search?q=meow&ie=utf-8&oe=utf-8")).toBe("www.google.com");
+    });
+    it("should properly format links", function() {
+      expect(linkFilter("https://news.ycombinator.com/item?id=9127232")).toBe("news.ycombinator.com");
     });
     it("should format https scheme", function() {
-      expect(linkFilter("https://google.com")).toBe("google.com");
+      expect(linkFilter("https://angel.co/karmic-labs-1/jobs/31466-frontend-engineer")).toBe("angel.co");
+    });
+    it("should format https scheme", function() {
+      expect(linkFilter("https://get-hired-app.herokuapp.com/#/jobs/edit/13")).toBe("get-hired-app.herokuapp.com");
     });
   });
 });
