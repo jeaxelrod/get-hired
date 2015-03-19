3e30e397537558e50eaf5b88016d3d165ea39b00
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2015-03-18 17:52:18 -0700

    Add email validation to contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact < ActiveRecord::Base
+  validates :email, email: true
+
   belongs_to :job_application
   belongs_to :user
   belongs_to :job
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -5,11 +5,13 @@ RSpec.describe Contact, type: :model do
   it "has a valid factory" do
     expect(FactoryGirl.create(:contact)).to be_valid
   end
+
   it "belongs to a user" do
     contact = FactoryGirl.create(:contact)
 
     expect(contact.user).to be_valid
   end
+
   it "belongs to a job" do
     contact = FactoryGirl.create(:contact)
 
@@ -21,6 +23,26 @@ RSpec.describe Contact, type: :model do
 
     expect(contact.job_application).to be_valid
   end
-  it "validates emails"
-  it "validates phone numbers"
+
+  it "accepts valid emails" do
+    contact = FactoryGirl.create(:contact, email: "a@m.co")
+    expect(contact).to be_valid
+  end
+
+  it "is valid without an email" do
+    contact = FactoryGirl.create(:contact, email: "")
+    expect(contact).to be_valid
+  end
+
+  it "is invalid without a proper email" do
+    contact = FactoryGirl.create(:contact)
+    contact.email = "a.com"
+    expect(contact).to_not be_valid
+  end
+
+  it "is invalid without a propeer email" do
+    contact = FactoryGirl.create(:contact)
+    contact.email = "h@com"
+    expect(contact).to_not be_valid
+  end
 end
