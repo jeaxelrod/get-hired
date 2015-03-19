e6cb3e7a496d4f6aec67c753b6365e44e4d0c8ac
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2015-03-18 17:01:30 -0700

    Added instructions for a demo login

--- app/assets/javascripts/controllers/home_controller.js
+++ app/assets/javascripts/controllers/home_controller.js
@@ -1,6 +1,8 @@
 var app = angular.module('getHired');
 
-app.controller('HomeController', ['$scope', 'FlashService', 
-  function($scope, FlashService) {
+app.controller('HomeController', ['$scope', 'FlashService', 'AuthService',
+  function($scope, FlashService, AuthService) {
+    $scope.isLoggedIn = AuthService.isLoggedIn();
+    console.log($scope.isLoggedIn);
   }
 ]);
